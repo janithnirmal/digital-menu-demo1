5ab1f1a66b83d19ed7f28db5ee4d1a534ac99763
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-04-21 20:57:01 +0530

    db updates

--- db/backups/voodoo-pla-backup1-(userAccess).sql
+++ db/backups/voodoo-pla-backup1-(userAccess).sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
--- Host: localhost    Database: project-pla
+-- Host: localhost    Database: digital_menu_demo_1
 -- ------------------------------------------------------
 -- Server version	8.0.31
 
@@ -16,288 +16,139 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `gender`
+-- Table structure for table `admin`
 --
 
-DROP TABLE IF EXISTS `gender`;
+DROP TABLE IF EXISTS `admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `gender` (
-  `gender_id` int NOT NULL AUTO_INCREMENT,
-  `gender` varchar(45) NOT NULL,
-  PRIMARY KEY (`gender_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `gender`
---
-
-LOCK TABLES `gender` WRITE;
-/*!40000 ALTER TABLE `gender` DISABLE KEYS */;
-INSERT INTO `gender` VALUES (1,'male'),(2,'female'),(3,'other');
-/*!40000 ALTER TABLE `gender` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `project`
---
-
-DROP TABLE IF EXISTS `project`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `project` (
-  `project_id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) NOT NULL,
-  `description` text NOT NULL,
-  `starting_date` datetime NOT NULL,
-  `ending_date` datetime NOT NULL,
-  `recorded_date` datetime NOT NULL,
-  `project_status_id` int NOT NULL,
-  `projectcol` varchar(45) NOT NULL,
-  `project_user_id` int NOT NULL,
-  PRIMARY KEY (`project_id`),
-  KEY `fk_project_project_status1_idx` (`project_status_id`),
-  KEY `fk_project_user1_idx` (`project_user_id`),
-  CONSTRAINT `fk_project_project_status1` FOREIGN KEY (`project_status_id`) REFERENCES `project_status` (`project_status_id`),
-  CONSTRAINT `fk_project_user1` FOREIGN KEY (`project_user_id`) REFERENCES `user` (`user_id`)
+CREATE TABLE `admin` (
+  `mobile` varchar(10) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `verification_code` varchar(45) NOT NULL,
+  PRIMARY KEY (`mobile`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `project`
+-- Dumping data for table `admin`
 --
 
-LOCK TABLES `project` WRITE;
-/*!40000 ALTER TABLE `project` DISABLE KEYS */;
-/*!40000 ALTER TABLE `project` ENABLE KEYS */;
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+INSERT INTO `admin` VALUES ('0710902997','rmjanithnirmal@gmail.com','123123','000000000');
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `project_status`
+-- Table structure for table `availability`
 --
 
-DROP TABLE IF EXISTS `project_status`;
+DROP TABLE IF EXISTS `availability`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `project_status` (
-  `project_status_id` int NOT NULL AUTO_INCREMENT,
-  `project_status` varchar(45) NOT NULL,
-  PRIMARY KEY (`project_status_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `project_status`
---
-
-LOCK TABLES `project_status` WRITE;
-/*!40000 ALTER TABLE `project_status` DISABLE KEYS */;
-INSERT INTO `project_status` VALUES (1,'not started'),(2,'ongoing'),(3,'hold'),(4,'done'),(5,'removed'),(6,'cancelled');
-/*!40000 ALTER TABLE `project_status` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `reminder`
---
-
-DROP TABLE IF EXISTS `reminder`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `reminder` (
-  `reminder_id` int NOT NULL AUTO_INCREMENT,
-  `reminder` varchar(100) NOT NULL,
-  `remind_datetime` datetime NOT NULL,
-  `recorded_datetime` datetime NOT NULL,
-  `description` varchar(250) DEFAULT NULL,
-  `reminder_user_id` int NOT NULL,
-  PRIMARY KEY (`reminder_id`),
-  KEY `fk_reminder_user1_idx` (`reminder_user_id`),
-  CONSTRAINT `fk_reminder_user1` FOREIGN KEY (`reminder_user_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `reminder`
---
-
-LOCK TABLES `reminder` WRITE;
-/*!40000 ALTER TABLE `reminder` DISABLE KEYS */;
-/*!40000 ALTER TABLE `reminder` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `task`
---
-
-DROP TABLE IF EXISTS `task`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `task` (
-  `task_id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(150) NOT NULL,
-  `description` text,
-  `starting_datetime` datetime NOT NULL,
-  `ending_datetime` datetime NOT NULL,
-  `task_status_id` int NOT NULL,
-  `task_project_id` int NOT NULL,
-  `recorded_datetime` datetime NOT NULL,
-  PRIMARY KEY (`task_id`),
-  KEY `fk_task_task_status1_idx` (`task_status_id`),
-  KEY `fk_task_project1_idx` (`task_project_id`),
-  CONSTRAINT `fk_task_project1` FOREIGN KEY (`task_project_id`) REFERENCES `project` (`project_id`),
-  CONSTRAINT `fk_task_task_status1` FOREIGN KEY (`task_status_id`) REFERENCES `task_status` (`task_status_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `task`
---
-
-LOCK TABLES `task` WRITE;
-/*!40000 ALTER TABLE `task` DISABLE KEYS */;
-/*!40000 ALTER TABLE `task` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `task_status`
---
-
-DROP TABLE IF EXISTS `task_status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `task_status` (
-  `task_status_id` int NOT NULL AUTO_INCREMENT,
-  `task_status` varchar(45) NOT NULL,
-  PRIMARY KEY (`task_status_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `task_status`
---
-
-LOCK TABLES `task_status` WRITE;
-/*!40000 ALTER TABLE `task_status` DISABLE KEYS */;
-INSERT INTO `task_status` VALUES (1,'not started'),(2,'ongoing'),(3,'done'),(4,'failed'),(5,'canceled');
-/*!40000 ALTER TABLE `task_status` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `todo`
---
-
-DROP TABLE IF EXISTS `todo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `todo` (
-  `todo_id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(200) NOT NULL,
-  `description` varchar(500) DEFAULT NULL,
-  `recorded_datetime` datetime NOT NULL,
-  `due_datetime` datetime NOT NULL,
-  `todo_status_id` int NOT NULL,
-  `todo_user_id` int NOT NULL,
-  PRIMARY KEY (`todo_id`),
-  KEY `fk_todo_todo_status1_idx` (`todo_status_id`),
-  KEY `fk_todo_user1_idx` (`todo_user_id`),
-  CONSTRAINT `fk_todo_todo_status1` FOREIGN KEY (`todo_status_id`) REFERENCES `todo_status` (`todo_status_id`),
-  CONSTRAINT `fk_todo_user1` FOREIGN KEY (`todo_user_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `todo`
---
-
-LOCK TABLES `todo` WRITE;
-/*!40000 ALTER TABLE `todo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `todo` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `todo_status`
---
-
-DROP TABLE IF EXISTS `todo_status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `todo_status` (
-  `todo_status_id` int NOT NULL AUTO_INCREMENT,
-  `todo_status` varchar(45) NOT NULL,
-  PRIMARY KEY (`todo_status_id`)
+CREATE TABLE `availability` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `availability` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `todo_status`
+-- Dumping data for table `availability`
 --
 
-LOCK TABLES `todo_status` WRITE;
-/*!40000 ALTER TABLE `todo_status` DISABLE KEYS */;
-INSERT INTO `todo_status` VALUES (1,'todo'),(2,'done');
-/*!40000 ALTER TABLE `todo_status` ENABLE KEYS */;
+LOCK TABLES `availability` WRITE;
+/*!40000 ALTER TABLE `availability` DISABLE KEYS */;
+INSERT INTO `availability` VALUES (1,'available'),(2,'unavailable');
+/*!40000 ALTER TABLE `availability` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `user`
+-- Table structure for table `category`
 --
 
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `category`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(250) NOT NULL,
-  `first_name` varchar(45) NOT NULL,
-  `last_name` varchar(45) NOT NULL,
-  `mobile` varchar(10) NOT NULL,
-  `registed_datetime` datetime NOT NULL,
-  `password` text NOT NULL,
-  `salt` text NOT NULL,
-  `user_status_id` int NOT NULL,
-  `user_gender_id` int NOT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `email_UNIQUE` (`email`),
-  KEY `fk_user_status_idx` (`user_status_id`),
-  KEY `fk_user_gender1_idx` (`user_gender_id`),
-  CONSTRAINT `fk_user_gender1` FOREIGN KEY (`user_gender_id`) REFERENCES `gender` (`gender_id`),
-  CONSTRAINT `fk_user_status` FOREIGN KEY (`user_status_id`) REFERENCES `user_status` (`user_status_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COMMENT='		';
+CREATE TABLE `category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `category` varchar(100) NOT NULL,
+  `img_url` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `category`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'email@gmail.com','janith','nirmal','0710902997','2023-03-23 00:00:00','123123','123123',1,1),(2,'janith@gmail.com','janith','nirmal','0710902997','2023-03-29 05:45:53','48aea17fb381cb693e7797e435ccb83eeb9bcef3b161c5b786a8daf06cff251f4294bffebc938446359acab0cd4ea4ad1553e829221facd0bae413c1ea77f1e3','144682108364238339b28e24.25551971',1,1);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'potato',''),(2,'noodles',''),(3,'sandwich',''),(4,'chilli',''),(5,'burger',''),(6,'rice',''),(7,'nachos',''),(8,'pasta',''),(9,'roll',''),(10,'pizza','');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `user_status`
+-- Table structure for table `contact_data`
 --
 
-DROP TABLE IF EXISTS `user_status`;
+DROP TABLE IF EXISTS `contact_data`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user_status` (
-  `user_status_id` int NOT NULL AUTO_INCREMENT,
-  `user_status` varchar(45) NOT NULL,
-  PRIMARY KEY (`user_status_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+CREATE TABLE `contact_data` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(250) DEFAULT NULL,
+  `mobile1` varchar(10) DEFAULT NULL,
+  `mobile2` varchar(10) DEFAULT NULL,
+  `address` varchar(250) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `country` varchar(45) DEFAULT NULL,
+  `website` varchar(150) DEFAULT NULL,
+  `optional1` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user_status`
+-- Dumping data for table `contact_data`
 --
 
-LOCK TABLES `user_status` WRITE;
-/*!40000 ALTER TABLE `user_status` DISABLE KEYS */;
-INSERT INTO `user_status` VALUES (1,'not verified'),(2,'verified'),(3,'hold'),(4,'banned');
-/*!40000 ALTER TABLE `user_status` ENABLE KEYS */;
+LOCK TABLES `contact_data` WRITE;
+/*!40000 ALTER TABLE `contact_data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contact_data` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL,
+  `price` double NOT NULL,
+  `availability_id` int NOT NULL,
+  `category_id` int NOT NULL,
+  `added_datetime` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_item_availability_idx` (`availability_id`),
+  KEY `fk_item_category1_idx` (`category_id`),
+  CONSTRAINT `fk_item_availability` FOREIGN KEY (`availability_id`) REFERENCES `availability` (`id`),
+  CONSTRAINT `fk_item_category1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -309,4 +160,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-29  8:31:56
+-- Dump completed on 2023-04-21 20:56:42
