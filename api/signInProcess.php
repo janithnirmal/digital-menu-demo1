93943013b712bde4f75596a446aa18f33eeac1ec
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-04-21 21:51:42 +0530

    sign in  api added

--- api/signInProcess.php
+++ api/signInProcess.php
@@ -2,13 +2,12 @@
 // import backend processes
 require("../app/inputValidator.php");
 require("../app/dbQuery.php");
-require("../app/passwordEncryptor.php");
 
 // get the request data
 $request = $_POST["signInData"]; // comes as a JSON
 $requestObject = json_decode($request);
 // -> check if user has provided all the importat data signIn (everything you need from the user signIN in DB)
-$email = $requestObject->email;
+$mobile = $requestObject->slmobile;
 $password = $requestObject->password;
 // if other data exists to be send from frontend goes here
 
@@ -27,33 +26,19 @@ foreach ($validationArray as $key => $value) {
 
 // -> check if a user exist for the data that request provide -> if exist: proceed to the next step -> ifnot: error out that invalid credintial errorrs
 $database = new DB();
-$checkerQuery = "SELECT * FROM `user` WHERE `email`=? ";
-$preparedStatement1 = $database->prepare($checkerQuery, 's', array($email));
+$checkerQuery = "SELECT * FROM `admin` WHERE `mobile`=? AND `password`=?";
+$preparedStatement1 = $database->prepare($checkerQuery, 'ss', array($mobile, $password));
 $userDataRaw = $preparedStatement1->get_result();
 $userData = $userDataRaw->fetch_assoc();
 
 if (!$userDataRaw->num_rows) {
-    echo ("Wrong email address");
+    echo ("Invalid Inputs");
     die();
 }
-
-
-// -> get the salt from db for the email that provided
-$salt = $userData["salt"];
-$hash = $userData["password"];
-
-
-// -> generate the password hash and check the similerty with DB and Generated ones. -> if matches: procced to the next step -> ifnot: error out
-$passwordValidator = new PasswordHashVerifier();
-if (!$passwordValidator->isValid($password, $salt, $hash)) {
-    echo ('wrong password');
-    die();
-}
-
 
 // -> create a session and store data init
 session_start();
-$_SESSION["pla_user"] = $userData;
+$_SESSION["menudemo1admin"] = $userData;
 
 // -> generate the response and output the response
 echo ('login success');
