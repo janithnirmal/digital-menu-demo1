31e86a3467305b47ac9fa40849370d64dc48bf24
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2023-04-23 06:05:40 +0530

    product loading api added

--- app/dbQuery.php
+++ app/dbQuery.php
@@ -12,10 +12,10 @@ class DB
     {
         $host = 'localhost';
         $user = 'root';
-        $password = '#Apeamma2001'; //  madusha
-        $database = 'menu-demo-1-23.04.2023'; //  madusha
-        // $password = 'JanithNirmal12#$'; //  janith
-        // $database = 'digital_menu_demo_1'; //  janith
+        // $password = '#Apeamma2001'; //  madusha
+        // $database = 'menu-demo-1-23.04.2023'; //  madusha
+        $password = 'JanithNirmal12#$'; //  janith
+        $database = 'digital_menu_demo_1'; //  janith
 
 
 
